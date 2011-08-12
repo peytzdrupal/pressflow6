ec9255926449eae3ad98bd4c3be391a08839e830
Anders Hal <hal@peytz.dk>
2011-08-12 16:38:56 +0200

    Removed comment

--- sites/sites.php
+++ sites/sites.php
@@ -1,8 +1,5 @@
 <?php
 // This file is Peytz-specific, see http://drupal.org/node/231298
-
-// Support development URLs, nn.example-org.pressflow6.dev.peytz.dk, and
-// temporary production URLs, example-org.pressflow6.f2.peytz.dk.
 $hostname = $_SERVER['HTTP_HOST'];
 if (preg_match('/([^.]+)\.pressflow(\d+)\./', $hostname, $matches)) {
   $sites = array(
